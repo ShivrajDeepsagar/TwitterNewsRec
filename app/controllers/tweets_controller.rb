a9e7902968c45e33fb113bdfd24260282d9bc177
YunoLevi <sanjana.ganesh20@gmail.com>
2015-04-12 08:45:36 -0400

    Fetched google news from mechanize gem

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 #$:.unshift File.dirname(__FILE__) + '/../lib'
 require 'rubygems'
 require 'google-search'
+require 'mechanize'
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
   
@@ -50,6 +51,35 @@ class TweetsController < ApplicationController
          @kwords << w.word
        end
       respond_with(@tweets)
+    else
+      #Google Top Stories
+      url = 'https://news.google.co.in/news?pz=1&cf=all&ned=in&siidp=7055ecc1c1f503ce3ad494721c0998bd7f1d&ict=ln'
+      @list_top = get_list(url)
+      #Google Technology News
+      url = 'https://news.google.co.in/news/section?pz=1&cf=all&ned=in&topic=tc&siidp=51a8e0c9f1fb4c19a56f76c6e2c0a66a2d36&ict=ln'
+      @list_tech = get_list(url)
+      #India News
+      url = 'https://news.google.co.in/news/section?pz=1&cf=all&ned=in&topic=n&siidp=1127bfb9f7d3dcaa1254b0c6f3a3ce97677f&ict=ln'
+      @list_india = get_list(url)
+      #Business
+      url = 'https://news.google.co.in/news/section?pz=1&cf=all&ned=in&topic=b&siidp=3fd6f26fc6bb30089b5a88be24dd3d6c0dc0&ict=ln'
+      @list_business = get_list(url)
+      #World
+      url = 'https://news.google.co.in/news/section?pz=1&cf=all&ned=in&topic=w&siidp=fc764afcb2f14630e4fba44f94706e97146d&ict=ln'
+      @list_world = get_list(url)
+      #Entertainment
+      url = 'https://news.google.co.in/news/section?pz=1&cf=all&ned=in&topic=e&siidp=47f84be18e74bc1bf5d8cd20939cf87fcd98&ict=ln'
+      @list_entertainment = get_list(url)
+      #Sports
+      url = 'https://news.google.co.in/news/section?pz=1&cf=all&ned=in&topic=s&siidp=4142ba2ebe24aded8da3cfa58a202b3db4a7&ict=ln'
+      @list_sports = get_list(url)
+      #Science
+      url = 'https://news.google.co.in/news/section?pz=1&cf=all&ned=in&topic=snc&siidp=b300874a3c17eecdffe434d086fac5c626e6&ict=ln'
+      agent = Mechanize.new { |agent| agent.user_agent_alias = "Mac Safari" }
+      @list_science = get_list(url)
+      #Health
+      url = 'https://news.google.co.in/news/section?pz=1&cf=all&ned=in&topic=m&siidp=0b8f8f8472add2a9b8330d6195161ff45a12&ict=ln'
+      @list_health = get_list(url)
     end
     
   end
@@ -92,5 +122,14 @@ class TweetsController < ApplicationController
 
     def tweet_params
       params.require(:tweet).permit(:user_id, :body)
+    end
+    def get_list(link)
+      url = link
+      agent = Mechanize.new { |agent| agent.user_agent_alias = "Mac Safari" }
+      html = agent.get(url).body
+      html_doc = Nokogiri::HTML(html)
+      list = html_doc.xpath("//h2[@class='esc-lead-article-title']")
+      list_6 = list.first(6)
+      return list_6
     end
 end

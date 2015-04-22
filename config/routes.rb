b13de828aed1c7bd23ab53ff7ba031e22aa129b5
YunoLevi <sanjana.ganesh20@gmail.com>
2015-04-22 07:18:58 -0400

    Added category - scafolded

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :categories
+
   get '/auth/:provider/callback', to: 'sessions#create'
   get 'signout', to: 'sessions#destroy', as: 'signout'
   resources :tweets
-
+  resources :categories
   root to: "tweets#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407071811) do
+ActiveRecord::Schema.define(version: 20150422092800) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.string   "google_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "hashtags", force: true do |t|
     t.integer  "count"
